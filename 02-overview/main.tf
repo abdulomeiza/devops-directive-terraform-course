a7db580bb71f57f3a762646b72037155149a37d9
Abdul Omeiza <108739614+abdulomeiza@users.noreply.github.com>
2023-01-12 12:07:29 +0100

    aws instance update
    updating aws instance specification to meet the current requirement on aws

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -7,6 +7,7 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -14,4 +15,22 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
+  subnet_id      = aws_subnet.MY_SUBNET.id
 }
+
+resource "aws_subnet" "MY_SUBNET" {
+  vpc_id     = aws_vpc.FirstVPC1.id
+  cidr_block = "172.31.0.0/16"
+
+  tags = {
+    Name = "MY_SUBNET"
+  }
+}
+  resource "aws_vpc" "FirstVPC1" {
+  cidr_block = "172.31.0.0/16"
+
+   tags = {
+    Name = "FirstVPC1"
+  }
+}
+
